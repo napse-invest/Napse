ca1ab95c9597d0a415f702daa8eff1d284926ef3
Tom Jeannesson <tomjeannesson@gmail.com>
2023-06-24 18:33:09 +0200

    desktop app setup

--- frontend/public/electron.js
+++ frontend/public/electron.js
@@ -1,48 +1,60 @@
-const { app, BrowserWindow } = require('electron')
+const electron = require("electron");
+const path = require("path");
+const url = require("url");
+
+const { app } = electron;
+const { BrowserWindow } = electron;
+
+const { ipcMain } = require("electron");
+
+let mainWindow;
 
 function createWindow() {
   const startUrl = process.env.DEV
-    ? 'http://localhost:3000'
+    ? "http://localhost:3000"
     : url.format({
-        pathname: path.join(__dirname, '/../build/index.html'),
-        protocol: 'file:',
+        pathname: path.join(__dirname, "/../build/index.html"),
+        protocol: "file:",
         slashes: true,
       });
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
   mainWindow.loadURL(startUrl);
   process.env.DEV && mainWindow.webContents.openDevTools();
 
-  mainWindow.on('closed', function() {
+  mainWindow.on("closed", function () {
     mainWindow = null;
   });
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-ipcMain.on(
-    'EVENT_LISTENER_NAME', 
-    (event, args) => {
-      /* ---- Code to execute as callback ---- */
-    }
-  );
+// ------------------- set up event listeners here --------------------
 
-  ipcRenderer.send(
-    'BACKGROUND_PROCESS_START', 
-    { 
-      "Key1": 123,
-      "Key2": 456
-    }
-  );
+// This event listener will listen for request
+// from visible renderer process
+ipcMain.on("START_BACKGROUND_VIA_MAIN", (event, args) => {
+  /* ---- Code to execute as callback ---- */
+});
+
+// This event listener will listen for data being sent back
+// from the background renderer process
+ipcMain.on("MESSAGE_FROM_BACKGROUND", (event, args) => {
+  /* ---- Code to execute as callback ---- */
+});
