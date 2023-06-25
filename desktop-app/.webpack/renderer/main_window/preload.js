2a949425396d0565a464093424b9b25164eeb275
Tom Jeannesson <tomjeannesson@gmail.com>
2023-06-25 09:09:30 +0200

    rm requirements.txt

---
+++ desktop-app/.webpack/renderer/main_window/preload.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/preload.ts":
+/*!************************!*\
+  !*** ./src/preload.ts ***!
+  \************************/
+/***/ (() => {
+
+eval("// See the Electron documentation for details on how to use preload scripts:\n// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvcHJlbG9hZC50cyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9kZXNrdG9wLWFwcC8uL3NyYy9wcmVsb2FkLnRzPzA1NmEiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsNEVBQTRFO0FBQzVFLGdGQUFnRiIsInNvdXJjZXNDb250ZW50IjpbIi8vIFNlZSB0aGUgRWxlY3Ryb24gZG9jdW1lbnRhdGlvbiBmb3IgZGV0YWlscyBvbiBob3cgdG8gdXNlIHByZWxvYWQgc2NyaXB0czpcbi8vIGh0dHBzOi8vd3d3LmVsZWN0cm9uanMub3JnL2RvY3MvbGF0ZXN0L3R1dG9yaWFsL3Byb2Nlc3MtbW9kZWwjcHJlbG9hZC1zY3JpcHRzXG4iXX0=\n//# sourceURL=webpack-internal:///./src/preload.ts\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/preload.ts"]();
+/******/ 	
+/******/ })()
+;
