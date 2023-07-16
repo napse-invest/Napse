94c2ecb0e2ef0354902701d83ca7f02d0899cfef
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-07-16 11:09:30 +0200

    Typography file added

--- desktop-app/src/renderer/src/utils/mui/theme.ts
+++ desktop-app/src/renderer/src/utils/mui/theme.ts
@@ -1,6 +1,8 @@
 // Styles
 import { palette } from './palette'
+import { typography } from './typography'
 
 export const getTheme = (mode: string) => ({
-  palette: palette(mode)
+  palette: palette(mode),
+  typography: typography
 })
---
+++ desktop-app/src/renderer/src/utils/mui/typography.ts
@@ -0,0 +1,53 @@
+export const typography = {
+  fontFamily: 'Inter',
+  h1: {
+    fontSize: '3.5rem',
+    fontWeight: 700
+  },
+  h2: {
+    fontSize: '2.75rem',
+    fontWeight: 700
+  },
+  h3: {
+    fontSize: '2rem',
+    fontWeight: 700
+  },
+  h4: {
+    fontSize: '1.5rem',
+    fontWeight: 700
+  },
+  h5: {
+    fontSize: '1.25rem',
+    fontWeight: 700
+  },
+  h6: {
+    fontSize: '1rem',
+    fontWeight: 700
+    // opacity: 0.8,
+  },
+  h7: {
+    fontSize: '1rem',
+    fontWeight: 700,
+    opacity: 0.8
+  },
+  body1: {
+    fontSize: '1rem',
+    fontWeight: 400
+  },
+  body2: {
+    fontSize: '0.75rem',
+    fontWeight: 400
+  },
+  button: {
+    fontSize: '0.85rem',
+    fontWeight: 700
+  },
+  caption: {
+    fontSize: '0.75rem',
+    fontWeight: 400
+  },
+  overline: {
+    fontSize: '0.75rem',
+    fontWeight: 400
+  }
+}
