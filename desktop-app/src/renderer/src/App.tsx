02f758f9e2be6be042a6b16dd372fa183cc9bc98
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-07-16 11:06:16 +0200

    theme applied

--- desktop-app/src/renderer/src/App.tsx
+++ desktop-app/src/renderer/src/App.tsx
@@ -8,28 +8,34 @@ import MainLayout from './layouts/MainLayout'
 // Intl
 import { useIntl } from 'react-intl'
 
+// Theme
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { getTheme } from './utils/mui/theme'
+
 function App(): JSX.Element {
   const intl = useIntl()
   return (
-    <MainLayout>
-      <BasicCard
-        title={
-          <Stack direction="row" justifyContent="space-between">
-            {intl.formatMessage({
-              id: 'global.my-environments',
-              defaultMessage: 'My environments'
-            })}
-            <Button variant="contained">
+    <ThemeProvider theme={createTheme(getTheme())}>
+      <MainLayout>
+        <BasicCard
+          title={
+            <Stack direction="row" justifyContent="space-between">
               {intl.formatMessage({
-                id: 'global.create-environment',
-                defaultMessage: 'Create environment'
+                id: 'global.my-environments',
+                defaultMessage: 'My environments'
               })}
-            </Button>
-          </Stack>
-        }
-        body={<></>}
-      />
-    </MainLayout>
+              <Button variant="contained">
+                {intl.formatMessage({
+                  id: 'global.create-environment',
+                  defaultMessage: 'Create environment'
+                })}
+              </Button>
+            </Stack>
+          }
+          body={<></>}
+        />
+      </MainLayout>
+    </ThemeProvider>
   )
 }
 
