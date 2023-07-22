d8074ce1c5e9ebd5c84b182a3797b06a40c1cc26
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-07-22 22:33:53 +0200

    Fake login page

--- desktop-app/src/renderer/src/views/Auth/Login/index.tsx
+++ desktop-app/src/renderer/src/views/Auth/Login/index.tsx
@@ -1,8 +1,36 @@
-const PageLogin = (): JSX.Element => {
+// React
+import { FC, useState } from 'react'
+
+// Components
+import Input from '../../../components/TextFields/Input'
+import Password from '../../../components/TextFields/Password'
+
+// Mui
+import { Button, Stack } from '@mui/material'
+
+const PageLogin: FC = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
-    <div>
-      <h1>This is the login page</h1>
-    </div>
+    <Stack spacing={2}>
+      <Input
+        label="Email"
+        value={email}
+        onChange={(e): void => setEmail(e.target.value)}
+        fullWidth
+        variant="outlined"
+      />
+      <Password
+        label="Password"
+        value={password}
+        onChange={(e): void => setPassword(e.target.value)}
+        fullWidth
+        variant="outlined"
+      />
+      <Button variant="contained" fullWidth>
+        Login
+      </Button>
+    </Stack>
   )
 }
 
