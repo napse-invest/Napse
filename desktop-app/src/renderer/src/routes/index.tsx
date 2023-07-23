629065c119942044e2624d66d92c3d9d01c66d0a
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-07-23 13:17:40 +0200

    Improve typing for AppRoutes

--- desktop-app/src/renderer/src/routes/index.tsx
+++ desktop-app/src/renderer/src/routes/index.tsx
@@ -1,8 +1,14 @@
+// React
+import { FC } from 'react'
+
+// Router
 import { Route, HashRouter as Router, Routes } from 'react-router-dom'
+
+// Components
 import Login from 'src/renderer/src/views/Auth/Login'
 import Redirect from 'src/renderer/src/views/Redirect'
 
-const AppRoutes = (): JSX.Element => {
+const AppRoutes: FC = () => {
   return (
     <Router>
       <Routes>
