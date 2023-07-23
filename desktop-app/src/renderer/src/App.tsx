b7d35aacc9eb5217246fe60ff8ef2ba3cc8dabb8
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-07-23 16:38:11 +0200

    Add layout to the App component

--- desktop-app/src/renderer/src/App.tsx
+++ desktop-app/src/renderer/src/App.tsx
@@ -8,12 +8,17 @@ import Font from 'react-font'
 // Routes
 import AppRoutes from './routes'
 
+// Layout
+import MainLayout from './layouts/MainLayout'
+
 function App(): JSX.Element {
   const theme = createTheme(getTheme('light'))
   return (
     <ThemeProvider theme={theme}>
       <Font family={theme.typography.fontFamily || 'Roboto'}>
-        <AppRoutes />
+        <MainLayout>
+          <AppRoutes />
+        </MainLayout>
       </Font>
     </ThemeProvider>
   )
