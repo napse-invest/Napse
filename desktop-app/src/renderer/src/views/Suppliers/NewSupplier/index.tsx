9e1f460e91c3feff90f37fafda2f042a7e53c4a4
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-07-23 17:20:48 +0200

    Init handleSubmit when adding a new supplier

--- desktop-app/src/renderer/src/views/Suppliers/NewSupplier/index.tsx
+++ desktop-app/src/renderer/src/views/Suppliers/NewSupplier/index.tsx
@@ -1,5 +1,5 @@
 // React
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 // Mui
 import { Button, Stack, Typography } from '@mui/material'
@@ -9,12 +9,21 @@ import Input from 'src/renderer/src/components/TextFields/Input'
 import Password from 'src/renderer/src/components/TextFields/Password'
 
 const PageNewSuppliers: FC = () => {
+  const [name, setName] = useState('')
+  const [apiKey, setApiKey] = useState('')
+
+  const handleSubmit = (): void => {
+    console.log('JKD092JD0293')
+  }
+
   return (
     <Stack spacing={2}>
       <Typography variant="h4">Add new supplier</Typography>
-      <Input label="Name" />
-      <Password label="API key" />
-      <Button variant="contained">Add</Button>
+      <Input label="Name" value={name} onChange={(e): void => setName(e.target.value)} />
+      <Password label="API key" value={apiKey} onChange={(e): void => setApiKey(e.target.value)} />
+      <Button variant="contained" onClick={handleSubmit}>
+        Add
+      </Button>
     </Stack>
   )
 }
