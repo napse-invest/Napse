099ac8ba6ac6c7b435c72d01b1ff0a3245c38997
Xenepix <nico.faglin@gmail.com>
2023-08-31 09:04:44 +0200

    feat(panel): panels are now composants

--- desktop-app/renderer/pages/spaces/index.tsx
+++ desktop-app/renderer/pages/spaces/index.tsx
@@ -1,7 +1,6 @@
 import ContextHeader from '@/components/layout/contextHeader'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
-import ValuePanelCard from './test'
+import ValuePanelCard from '@/components/ui/panel/value_panel_card'
 
 function DisplaySpaces(): JSX.Element {
   const spaces = [
@@ -10,21 +9,21 @@ function DisplaySpaces(): JSX.Element {
       bots: 3,
       value: 1.01,
       change: 20.6,
-      tooltip: 'mdr'
+      tooltip: 'test'
     },
     {
       name: 'Space Mountain',
       bots: 152,
       value: 152163231.89,
       change: 0,
-      tooltip: 'mdr'
+      tooltip: 'test'
     },
     {
       name: 'Zero Space',
       bots: 8,
       value: 45621.89,
       change: -6.468,
-      tooltip: 'mdr'
+      tooltip: 'test'
     }
   ]
 
