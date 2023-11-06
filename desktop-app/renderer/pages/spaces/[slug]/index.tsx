781bfc7b7e156b1a97bb9e6d50cbe525bc0bc117
Xenepix38 <nico.faglin@gmail.com>
2023-11-06 21:33:28 +0100

    fix(space): setup detail and start graph

--- desktop-app/renderer/pages/spaces/[slug]/index.tsx
+++ desktop-app/renderer/pages/spaces/[slug]/index.tsx
@@ -1,343 +1,90 @@
+import { RetrievedNapseSpace, retrieveSpace } from '@/api/spaces/spaces'
 import ContextHeader from '@/components/layout/contextHeader'
-import { Button } from '@/components/ui/button'
+import DefaultPageLayout from '@/components/layout/defaultPageLayout'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { ScrollArea } from '@/components/ui/scroll-area'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { useToast } from '@/components/ui/use-toast'
-import { AreaChart, DonutChart } from '@tremor/react'
-import React, { useState } from 'react'
+import { standardUrlPartial } from '@/lib/queryParams'
+import { AreaChart } from '@tremor/react'
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { fakeDashboardData } from './fakeDashboardData'
 
-import StatCard from '@/components/custom/statCard'
-import { Currency, getAssetColumns } from './assetsTable/columns'
+export default function Space(): JSX.Element {
+  const searchParams = useSearchParams()
+  const router = useRouter()
 
-import CustomTable from '@/components/custom/table'
-import { OperationDataTable } from './transactionHistTable/data-table'
-
-const cities = [
-  {
-    name: 'New York',
-    sales: 9800
-  },
-  {
-    name: 'London',
-    sales: 4567
-  },
-  {
-    name: 'Hong Kong',
-    sales: 3908
-  },
-  {
-    name: 'San Francisco',
-    sales: 2400
-  },
-  {
-    name: 'Singapore',
-    sales: 1908
-  },
-  {
-    name: 'Zurich',
-    sales: 1398
-  }
-]
-
-const valueFormatter = (number: number) =>
-  `$ ${Intl.NumberFormat('us').format(number).toString()}`
-
-const chartdata = [
-  { index: 0, wallet: 1000 },
-  { index: 1, wallet: 174 },
-  { index: 2, wallet: -394 },
-  { index: 3, wallet: 1518 },
-  { index: 4, wallet: 1720 },
-  { index: 5, wallet: 3088 },
-  { index: 6, wallet: 2769 },
-  { index: 7, wallet: 4329 },
-  { index: 8, wallet: 6703 },
-  { index: 9, wallet: 4626 },
-  { index: 10, wallet: 3762 },
-  { index: 11, wallet: 5867 },
-  { index: 12, wallet: 4915 },
-  { index: 13, wallet: 4691 },
-  { index: 14, wallet: 3655 },
-  { index: 15, wallet: 5941 },
-  { index: 16, wallet: 6069 },
-  { index: 17, wallet: 7644 },
-  { index: 18, wallet: 7206 },
-  { index: 19, wallet: -6069 },
-  { index: 20, wallet: 4222 },
-  { index: 21, wallet: 2336 },
-  { index: 22, wallet: 1302 },
-  { index: 23, wallet: 1875 },
-  { index: 24, wallet: 2000 },
-  { index: 25, wallet: 2210 },
-  { index: 26, wallet: 1446 },
-  { index: 27, wallet: 1194 },
-  { index: 28, wallet: -474 },
-  { index: 29, wallet: 441 }
-]
-const dataFormatter = (number: number) =>
-  `${Intl.NumberFormat('us').format(number).toString()}%`
-interface Stat {
-  title: string
-  metric: number
-  delta: number
-}
-
-const GraphCardComponent = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ ...props }, ref) => {
-  return (
-    <Card {...props} ref={ref} className=" pb-6">
-      <CardHeader className="flex flex-row items-center justify-between space-y-0">
-        <CardTitle className="text-sm font-normal">MY GRAPH</CardTitle>
-      </CardHeader>
-      <CardContent className="  pb-0">
-        <AreaChart
-          className="mx-auto h-96 "
-          data={chartdata}
-          index="index"
-          categories={['wallet']}
-          colors={['emerald']}
-          curveType="monotone"
-          startEndOnly={true}
-          // valueFormatter={dataFormatter}
-          yAxisWidth={40}
-        />
-      </CardContent>
-    </Card>
-  )
-})
-
-GraphCardComponent.displayName = 'GraphCardComponent'
-
-function getData(): Currency[] {
-  return [
-    {
-      amount: 100,
-      ticker: 'BUSD'
-    },
-    {
-      amount: 10,
-      ticker: 'USDT'
-    },
-    {
-      amount: 1000,
-      ticker: 'Matic'
-    },
-    {
-      amount: 100,
-      ticker: 'BUSD'
-    },
-    {
-      amount: 10,
-      ticker: 'USDT'
-    },
-    {
-      amount: 1000,
-      ticker: 'Matic'
-    },
-    {
-      amount: 100,
-      ticker: 'BUSD'
-    },
-    {
-      amount: 10,
-      ticker: 'USDT'
-    },
-    {
-      amount: 1000,
-      ticker: 'Matic'
-    },
-    {
-      amount: 100,
-      ticker: 'BUSD'
-    },
-    {
-      amount: 10,
-      ticker: 'USDT'
-    },
-    {
-      amount: 1000,
-      ticker: 'Matic'
-    },
-    {
-      amount: 100,
-      ticker: 'BUSD'
-    },
-    {
-      amount: 10,
-      ticker: 'USDT'
-    },
-    {
-      amount: 1000,
-      ticker: 'Matic'
+  const spaceID: string = searchParams.get('space') || ''
+  // const spaceID: string = 'wrongSpaceUUID'
+  const [space, setSpace] = useState<RetrievedNapseSpace>()
+  const toast = useToast()
+  useEffect(() => {
+    const fetchSpace = async () => {
+      try {
+        const response = await retrieveSpace(searchParams, spaceID)
+        setSpace(response.data)
+      } catch (error) {
+        console.error(error)
+        // go to the previous page
+        router.push(
+          standardUrlPartial('/spaces/', null, { space: '' }, searchParams)
+        )
+        toast.toast({
+          // variant: 'destructive',
+          title: 'Something went wrong...',
+          description: 'There was a problem with this space'
+        })
+      }
     }
-  ]
-}
-
-export default function Spaces(): JSX.Element {
-  // const { name } = useSelector((state: RootStateType) => state.headerState)
-
-  let spaces: Array<Stat> = [
-    {
-      title: 'Stat 1',
-      metric: 5,
-      delta: 0
-    },
-    {
-      title: 'Stat 2',
-      metric: 42,
-      delta: 6
-    },
-    {
-      title: 'Stat 3',
-      metric: 5,
-      delta: -2
+    if (searchParams.get('server')) {
+      fetchSpace()
     }
-  ]
-  const [flip, setFlip] = useState(false)
-  function toggleFlip() {
-    setFlip(!flip)
-  }
-  const { toast } = useToast()
-  const data = getData()
+  }, [spaceID, searchParams])
+
   return (
     <ContextHeader isBot>
-      <div className="mx-auto my-10 max-w-screen-xl px-10">
-        {/* <div className="hidden flex-col md:flex"> */}
-        {/* <div className="flex-1 space-y-4 p-8 pt-6"> */}
-        <Tabs defaultValue="space" className="">
+      <DefaultPageLayout
+        header={space ? space.name : ''}
+        description={
+          'Here is an overview of all your spaces. A space make it easy to manage your money.'
+        }
+      >
+        <Tabs defaultValue="dashboard" className="w-[400px]">
           <TabsList>
-            <TabsTrigger value="space">Space</TabsTrigger>
-            <TabsTrigger value="wallet">Wallet</TabsTrigger>
-            <TabsTrigger value="fleets">Fleets</TabsTrigger>
+            <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
+            <TabsTrigger value="wallet" disabled>
+              Wallet
+            </TabsTrigger>
+            <TabsTrigger value="operations" disabled>
+              Operations
+            </TabsTrigger>
+            <TabsTrigger value="fleets" disabled>
+              Fleets
+            </TabsTrigger>
           </TabsList>
-          <TabsContent value="space">
-            <div className="my-10 grid max-w-screen-xl gap-6 lg:grid-cols-3">
-              {spaces.map((space, index) => (
-                <StatCard
-                  key={index}
-                  title={space.title}
-                  metric={space.metric}
-                  delta={space.delta}
+          <TabsContent
+            value="dashboard"
+            className="flex flex-col bg-red-800 w-full"
+          >
+            <Card className="flex flex-col bg-red-300 w-full">
+              <CardHeader>
+                <CardTitle>Dashboard</CardTitle>
+              </CardHeader>
+              <CardContent className="flex-1">
+                <AreaChart
+                  className="mt-4 h-80"
+                  data={fakeDashboardData}
+                  categories={['value']}
+                  index="date"
+                  colors={['indigo', 'fuchsia']}
+                  yAxisWidth={60}
                 />
-                // <ValuePanelCard
-                //   key={index}
-                //   title={space.name}
-                //   value={space.value}
-                //   delta={space.delta}
-                //   // tooltip={space.tooltip}
-                //   onClick={() => {}}
-                //   // badge={String(space.fleet_count) + ' fleets'}
-                // />
-              ))}
-            </div>
-            <div className=" my-10 h-24 max-w-screen-xl justify-center">
-              <GraphCardComponent />
-            </div>
+              </CardContent>
+            </Card>
           </TabsContent>
-          <TabsContent value="wallet">
-            <div className=" my-10 grid max-w-screen-xl gap-6">
-              <Card className="">
-                <CardHeader className="mt-0 flex flex-row justify-between">
-                  <CardTitle className="">Transaction history</CardTitle>
-                  <Button
-                    className=""
-                    variant="outline"
-                    size="icon"
-                    onClick={toggleFlip}
-                  >
-                    FLIP
-                  </Button>
-                </CardHeader>
-                <CardContent className="grid grid-cols-8 justify-between gap-6">
-                  <div className="col-span-5">
-                    {/* <GraphCardComponent /> */}
-
-                    <div
-                      className={`preserve-3d relative h-full w-full duration-1000 ${
-                        flip ? 'flip-x-180' : ''
-                      }`}
-                    >
-                      <div className="backface-hidden absolute z-0 h-full w-full">
-                        <GraphCardComponent className="" />
-                      </div>
-                      <div className="flip-x-180 backface-hidden absolute h-full w-full">
-                        {/* <GraphCardComponent /> */}
-                        <ScrollArea className=" h-[30rem] rounded-md border">
-                          <CustomTable
-                            columns={getAssetColumns(toast)}
-                            data={data}
-                          />
-                        </ScrollArea>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-span-3">
-                    <Card className="py-8">
-                      <CardHeader>
-                        <CardTitle />
-                      </CardHeader>
-                      <CardContent className="">
-                        <DonutChart
-                          className="my-10"
-                          data={cities}
-                          category="sales"
-                          index="name"
-                          valueFormatter={valueFormatter}
-                          colors={[
-                            'slate',
-                            'violet',
-                            'indigo',
-                            'rose',
-                            'cyan',
-                            'amber'
-                          ]}
-                        />
-                        <div className="mx-10 mt-20 flex flex-row justify-between gap-8">
-                          <Button
-                            className="flex-1"
-                            variant="secondary"
-                            size="lg"
-                            onClick={() => {
-                              console.log('invest1')
-                              toast({
-                                title: 'Scheduled: Catch up',
-                                description:
-                                  'Friday, February 10, 2023 at 5:57 PM'
-                              })
-                              console.log('invest2')
-                            }}
-                          >
-                            Invest
-                          </Button>
-                          <Button
-                            className="flex-1"
-                            variant="secondary"
-                            size="lg"
-                            onClick={() => {
-                              console.log('withdraw')
-                            }}
-                          >
-                            Withdraw
-                          </Button>
-                        </div>
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CardContent>
-              </Card>
-              <OperationDataTable
-                columns={getAssetColumns(toast)}
-                data={data}
-              />
-            </div>
-          </TabsContent>
-          <TabsContent value="fleets">fleet </TabsContent>
         </Tabs>
-      </div>
-      {/* </div> */}
+      </DefaultPageLayout>
     </ContextHeader>
   )
 }
