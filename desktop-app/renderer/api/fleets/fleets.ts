034c34513ce647d8adf3222f305e7c43539c6576
Xenepix <nico.faglin@gmail.com>
2023-12-24 12:51:20 +0100

    fleet: add fleet endpoints

--- desktop-app/renderer/api/fleets/fleets.ts
+++ desktop-app/renderer/api/fleets/fleets.ts
@@ -1,6 +1,26 @@
-export interface Fleet{
+import { request } from 'api/request'
+import { AxiosResponse } from 'axios'
+import { useSearchParams } from 'next/navigation'
+
+interface Clusters {
+  bot_strategy: string
+  bot_name: string
+  share: number
+  breakpoint: number
+  autoscale: boolean
+}
+
+export interface Fleet {
   name: string
   uuid: string
   value: number
   bot_count: number
+  clusters?: Clusters[]
+}
+
+export async function listFleets(
+  searchParams: ReturnType<typeof useSearchParams>
+): Promise<AxiosResponse<Fleet[]>> {
+  const response = await request(searchParams, 'GET', '/api/fleet/')
+  return response as AxiosResponse<Fleet[]>
 }
--- desktop-app/renderer/pages/fleets/index.tsx
+++ desktop-app/renderer/pages/fleets/index.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import { Fleet, listFleets } from '@/api/fleets/fleets'
+import { Key, getCurrentKey } from '@/api/key/key'
 import ContextHeader from '@/components/layout/contextHeader'
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 export default function Fleets(): JSX.Element {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const [Fleets, setFleets] = useState<Fleet[]>([])
+  const [currentKey, setCurrentKey] = useState<Key>()
+
+  useEffect(() => {
+    async function fetchFleets() {
+      try {
+        const response = await listFleets(searchParams)
+        setFleets(response.data)
+      } catch (error) {
+        console.error(error)
+        setFleets([])
+      }
+    }
+    const fetchCurrentKey = async () => {
+      try {
+        const response = await getCurrentKey(searchParams)
+        setCurrentKey(response)
+      } catch (error) {
+        console.error(error)
+        setCurrentKey(undefined)
+      }
+    }
+    if (searchParams.get('server')) {
+      fetchFleets()
+      fetchCurrentKey()
+    }
+  }, [searchParams])
+  console.log(Fleets)
   return (
     <ContextHeader isBot>
       <></>
