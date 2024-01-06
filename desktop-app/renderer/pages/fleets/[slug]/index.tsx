0f3ec154edaac07607797de9186d16645ce911d0
Xenepix <Firenix.nex@gmail.com>
2024-01-06 17:59:36 +0100

    refactor(space): externalize getKeyData method for fleet dashboard

--- desktop-app/renderer/pages/fleets/[slug]/index.tsx
+++ desktop-app/renderer/pages/fleets/[slug]/index.tsx
@@ -1,8 +1,16 @@
+import { RetrievedFleet } from '@/api/fleets/fleets'
 import ContextHeader from '@/components/layout/contextHeader'
 import DefaultPageLayout from '@/components/layout/defaultPageLayout'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useState } from 'react'
 
 export default function Fleet(): JSX.Element {
-  // TODO: implement or move this file
+  const searchParams = useSearchParams()
+  const router = useRouter()
+
+  const fleetID: string = searchParams.get('fleet') || ''
+  const [fleet, setFleet] = useState<RetrievedFleet>()
+
   return (
     <ContextHeader isBot>
       <DefaultPageLayout
