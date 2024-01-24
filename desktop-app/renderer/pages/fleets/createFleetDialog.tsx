e3c0d4848be0e8460a42852b3a647625bfaca48a
Xenepix <Firenix.nex@gmail.com>
2024-01-24 22:52:05 +0100

    refactor(fleet): move form to dialog file

--- desktop-app/renderer/pages/fleets/createFleetDialog.tsx
+++ desktop-app/renderer/pages/fleets/createFleetDialog.tsx
@@ -6,8 +6,51 @@ import { useSearchParams } from 'next/navigation'
 import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { PlusIcon } from '@radix-ui/react-icons'
-import FleetForm from './createFleetForm'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+import { Cluster } from '@/api/fleets/fleets'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from '@/components/ui/carousel'
+import {
+  DialogDescription,
+  DialogHeader,
+  DialogTitle
+} from '@/components/ui/dialog'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
+import { Separator } from '@/components/ui/separator'
+import ClusterDataTable from './clusterDataTable'
+import CreateClusterDialog from './createclusterDialog'
+
+const FleetSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: 'You have to give a name.' })
+    .max(64, { message: 'The name cannot exceed 64 characters.' }),
+  space: z.string()
+})
 
 export default function CreateFleetDialog({
   fleets,
@@ -21,7 +64,7 @@ export default function CreateFleetDialog({
   const searchParams = useSearchParams()
   const [possibleSpaces, setPossibleSpaces] = useState<NapseSpace[]>([])
   const [fleet, setFleet] = useState<BaseFleet>()
-
+  const [Clusters, setClusters] = useState<Cluster[]>([])
   useEffect(() => {
     const fetchPossibleSpaces = async () => {
       try {
@@ -37,13 +80,23 @@ export default function CreateFleetDialog({
     }
   }, [searchParams])
 
-  const SpacePossibilitiesSelection = possibleSpaces.reduce(
+  const napseSpacePossibilitiesSelection = possibleSpaces.reduce(
     (obj, item) => {
       obj[item.name] = item.uuid
       return obj
     },
     {} as { [key: string]: string }
   )
+  const form = useForm<z.infer<typeof FleetSchema>>({
+    resolver: zodResolver(FleetSchema),
+    defaultValues: {
+      name: 'Fleet Name',
+      space: Object.values(napseSpacePossibilitiesSelection)[0]
+    }
+  })
+  function onSubmit(values: z.infer<typeof FleetSchema>) {
+    console.log(FleetSchema)
+  }
 
   return (
     <Dialog>
@@ -57,10 +110,93 @@ export default function CreateFleetDialog({
           Add new
         </Button>
       </DialogTrigger>
-      {/* <DialogContent className="sm:max-w-[425px]"> */}
-      {/* <DialogContent className="max-w"> */}
       <DialogContent className="">
-        <FleetForm />
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+            <Carousel className="">
+              <CarouselContent>
+                <CarouselItem className="">
+                  <DialogHeader className="">
+                    <DialogTitle>Add a new Fleet</DialogTitle>
+                    <DialogDescription>
+                      Add a new fleet will allow you to setup bots & invest on
+                      them.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid grid-cols-5">
+                    <div></div>
+                    <Separator className="col-span-3 my-8" />
+                    <div></div>
+                  </div>
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem className="mx-2 mb-5 flex flex-col space-y-1.5">
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="fleet" {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="space"
+                    render={({ field }) => (
+                      <FormItem>
+                        <div className="mx-2 mb-5 flex flex-col space-y-1.5">
+                          <FormLabel>Space</FormLabel>
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <FormControl>
+                              <SelectTrigger>
+                                <SelectValue placeholder="Select your space" />
+                              </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                              {Object.entries(
+                                napseSpacePossibilitiesSelection
+                              ).map(([name, value]) => (
+                                <SelectItem
+                                  key={name}
+                                  value={value}
+                                  // disabled={false}
+                                >
+                                  {name}
+                                </SelectItem>
+                              ))}
+                            </SelectContent>
+                          </Select>
+                        </div>
+                      </FormItem>
+                    )}
+                  />
+                </CarouselItem>
+                <CarouselItem>
+                  <DialogHeader className="">
+                    <DialogTitle>Add clusters to your fleet</DialogTitle>
+                    <DialogDescription>
+                      Clusters will enable fleets to manage bots.
+                    </DialogDescription>
+                  </DialogHeader>
+
+                  <ScrollArea className="mb-3 mt-8 h-44 w-full rounded-md border">
+                    <ClusterDataTable data={Clusters} />
+                  </ScrollArea>
+                  <div className="flex flex-row justify-between">
+                    <CreateClusterDialog possibleTemplateBots={[]} />
+                    <Button type="submit">Create</Button>
+                  </div>
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </form>
+        </Form>
       </DialogContent>
       <DialogClose id="close-button" />
     </Dialog>
